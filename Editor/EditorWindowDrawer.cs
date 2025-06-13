d16119782719ffdb16041d9cefc60ce0cae1bdab
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-06-13 09:03:22 +0200

    ⠀⠀⠀⠀⠀

---
+++ Editor/EditorWindowDrawer.cs
@@ -0,0 +1,169 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System;
+
+namespace UnityEssentials
+{
+    public class EditorWindowDrawer : EditorWindow
+    {
+        public enum GUISkin
+        {
+            None,
+            Box,
+            Window,
+        }
+
+        private static Vector2 s_minSize = new(300, 400);
+        private static Vector2 s_position = new(Screen.width / 2f, Screen.height / 2f);
+
+        public EditorWindowDrawer Window(string title, Vector2? minSize = null, Vector2? position = null)
+        {
+            minSize ??= s_minSize;
+            position ??= s_position;
+
+            var window = GetWindow<EditorWindowDrawer>();
+            window.titleContent = new GUIContent(title);
+            window.minSize = minSize.Value;
+            window.position = new Rect(position.Value.x, position.Value.y, minSize.Value.x, minSize.Value.y);
+            window.Show();
+            return this;
+        }
+
+        public EditorWindowDrawer Utility(string title, Vector2? minSize = null, Vector2? position = null)
+        {
+            minSize ??= s_minSize;
+            position ??= s_position;
+
+            var window = CreateInstance<EditorWindowDrawer>();
+            window.titleContent = new GUIContent(title);
+            window.minSize = minSize.Value;
+            window.position = new Rect(position.Value.x, position.Value.y, minSize.Value.x, minSize.Value.y);
+            window.ShowUtility();
+            return this;
+        }
+
+        public EditorWindowDrawer Popup(string title, Vector2? minSize = null, Vector2? position = null)
+        {
+            minSize ??= s_minSize;
+            position ??= s_position;
+
+            var window = CreateInstance<EditorWindowDrawer>();
+            window.titleContent = new GUIContent(title);
+            window.minSize = minSize.Value;
+            window.position = new Rect(position.Value.x, position.Value.y, minSize.Value.x, minSize.Value.y);
+            window.ShowPopup();
+            return this;
+        }
+
+        public EditorWindowDrawer Modal(string title, Vector2? minSize = null, Vector2? position = null)
+        {
+            minSize ??= s_minSize;
+            position ??= s_position;
+
+            var window = CreateInstance<EditorWindowDrawer>();
+            window.titleContent = new GUIContent(title);
+            window.minSize = minSize.Value;
+            window.position = new Rect(position.Value.x, position.Value.y, minSize.Value.x, minSize.Value.y);
+            window.ShowModalUtility();
+            return this;
+        }
+
+        private Action _headerAction;
+        private GUISkin _headerSkin;
+        public EditorWindowDrawer SetHeader(Action header, GUISkin skin = GUISkin.None)
+        {
+            _headerAction = header;
+            _headerSkin = skin;
+            return this;
+        }
+
+        private Action _bodyAction;
+        private GUISkin _bodySkin;
+        private Vector2 _bodyScrollPosition;
+        public EditorWindowDrawer SetBody(Action body, out Vector2 scrollPosition, GUISkin skin = GUISkin.None)
+        {
+            scrollPosition = _bodyScrollPosition;
+            _bodyAction = body;
+            _bodySkin = skin;
+            return this;
+        }
+
+        private Action _footerAction;
+        private GUISkin _footerSkin;
+        public EditorWindowDrawer SetFooter(Action footer, GUISkin skin = GUISkin.None)
+        {
+            _footerAction = footer;
+            _footerSkin = skin;
+            return this;
+        }
+
+        private void OnGUI()
+        {
+            BeginWindow();
+
+            BeginHeader(_headerSkin);
+            _headerAction?.Invoke();
+            EndHeader();
+
+            BeginBody(_bodySkin, ref _bodyScrollPosition);
+            _bodyAction?.Invoke();
+            EndBody();
+
+            BeginFooter(_footerSkin);
+            _footerAction?.Invoke();
+            EndFooter();
+
+            EndWindow();
+        }
+
+        private static void BeginWindow() =>
+            GUILayout.BeginVertical();
+
+
+        private static void EndWindow() =>
+            GUILayout.EndVertical();
+
+        private static void BeginHeader(GUISkin skin) =>
+            GUILayout.BeginVertical(skin switch
+            {
+                GUISkin.None => GUIStyle.none,
+                GUISkin.Box => GUI.skin.box,
+                GUISkin.Window => GUI.skin.window,
+            });
+
+        private static void EndHeader() =>
+            GUILayout.EndVertical();
+
+        private static void BeginBody(GUISkin skin, ref Vector2 scrollPosition)
+        {
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+            GUILayout.BeginVertical(skin switch
+            {
+                GUISkin.None => GUIStyle.none,
+                GUISkin.Box => GUI.skin.box,
+                GUISkin.Window => GUI.skin.window,
+            });
+            if(skin == GUISkin.Window)
+                GUILayout.Space(-18);
+        }
+
+        private static void EndBody()
+        {
+            GUILayout.EndVertical();
+            EditorGUILayout.EndScrollView();
+        }
+
+        private static void BeginFooter(GUISkin skin) =>
+            GUILayout.BeginVertical(skin switch
+            {
+                GUISkin.None => GUIStyle.none,
+                GUISkin.Box => GUI.skin.box,
+                GUISkin.Window => GUI.skin.window,
+            });
+
+        private static void EndFooter() =>
+            GUILayout.EndVertical();
+    }
+}
+#endif
---
+++ Editor/Examples/SimpleWindowExample.cs
@@ -0,0 +1,28 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using UnityEssentials;
+
+namespace Examples.EditorWindow
+{
+    public class ExampleWindow
+    {
+        [MenuItem("Tools/Examples/Simple Example Window", priority = 10000)]
+        public static void ShowWindow() =>
+            new EditorWindowDrawer()
+                .Window("Example")
+                .SetHeader(Header)
+                .SetBody(Body, out _)
+                .SetFooter(Footer);
+
+        public static void Header() =>
+            GUILayout.Label("Header");
+
+        public static void Body() =>
+            GUILayout.Label("Body");
+
+        public static void Footer() =>
+            GUILayout.Label("Footer");
+    }
+}
+#endif
